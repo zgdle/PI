6acb27f2b05db0b91bbb4efc93539a2e459176cb
ckyeon <qsky8520@gmail.com>
2023-09-04 02:05:01 +0900

    initial commit

---
+++ PI/PIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIApp.swift
+//  PI
+//
+//  Created by 조기연 on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct PIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
